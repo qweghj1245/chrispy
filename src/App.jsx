a22b4bc03a174a012eba5308afd45988dee97801
mazusu <qweghj1245@gmail.com>
2023-03-28 13:40:13 +0800

    feat(): test

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 import React from "react";
 import "./App.css";
 import logo from "./logo.svg";
-import { RefreshButton } from "@redreamerlab/uikit";
-
-// console.log(add(1, 2));
-// console.log(minus(222, 111));
+import { Footer } from "@qweghj1245/chrispy-ch";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>6667</div>
-        <RefreshButton onRefresh={() => {}} />
-        {/* <Body>
-          <div>123123213</div>
-        </Body> */}
+        <Footer title="testfooter" />
       </header>
     </div>
   );
